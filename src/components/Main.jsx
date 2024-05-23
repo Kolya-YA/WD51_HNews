e84e5d017164f01f3cc019a420f75418a00ac6d1
janghw33 <janghw33@gmail.com>
2024-05-23 22:31:57 +0200

    pagination css update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,9 +5,9 @@ const Main = ({ news }) => {
 
     return (
         <main className="container mx-auto p-4">
-            <Paginator />
             <h2 className="text-2xl font-bold text-gray-600">News / Search result</h2>
             <p className="mb-4 text-end text-gray-800">Qty of news: {news.length}</p>
+            <Paginator />
             <ul className="bg-white shadow rounded-lg divide-y divide-gray-200">
                 {news?.map((n, i) => (
                     <NewsItem news={n} i={i} key={n.objectID} />
