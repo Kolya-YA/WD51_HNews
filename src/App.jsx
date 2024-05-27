d296cc3b60691b72373a15351f94ce44019f3a40
janghw33 <janghw33@gmail.com>
2024-05-27 10:20:29 +0200

    recover

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,55 @@
-import { useEffect, useState } from "react";
-import { hits as news } from "./data/hnews.json";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import { useEffect, useState } from 'react'
+import { hits as news } from './data/hnews.json'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   const [newsList, setNewsList] = useState([])
-  const [articles, setArticles] = useState([]);
-  const [query, setQuery] = useState('react');
+  const [hiddenNews, setHiddenNews] = useState("React");
 
-  //const [searchNews, setSearchNews] = useState("");
-  //=> {
-  // const storedHiddenNews = localStorage.getItem("hiddenNews") || "[]";
-  // return JSON.parse(storedHiddenNews);
-  //});
+  // const [hiddenNews, setHiddenNews] = useState(() => {
+  //   const storedHiddenNews = localStorage.getItem('hiddenNews') || '[]';
+  //   return JSON.parse(storedHiddenNews);
+  // })
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
-        const result = await response.json();
-
-        setTimeout(() => {
-          setNewsList(result.hits)
-        }, 3000) // Fake delay to show loader
-      } catch (error) {
-        console.error('Error fetching the data', error);
-      }
-    };
-
-    fetchData();
-  }, [query]);
-
-  useEffect(() => {
-    localStorage.setItem('hiddenNews', JSON.stringify(hiddenNews))
-  }, [hiddenNews])
-
-  const handleSearch = (e) => {
-    //e.preventDefault();
-    // const search = e.target.search.value;
-    setNewsList((n) => n);
+  //   localStorage.setItem('hiddenNews', JSON.stringify(hiddenNews))
+  // }, [hiddenNews])
+  const fetchNewsList = async () => {
+    try {
+      const response = await fetch(
+        `https://hn.algolia.com/api/v1/search?query=${hiddenNews}`
+      );
+      const data = await response.json();
+      setNewsList(data.hits);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
+  fetchNewsList();
+}, [hiddenNews]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setNewsList(news)
+    }, 4000) // Fake delay to show loader
+  }, [])
+  
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // const search = e.target.search.value;
+    setNewsList(n => n)
+   }
+  
   return (
     <>
-     {/* <div>
-      <h1>Hacker News Search</h1>
-      <input 
-        type="text" 
-        value={query} 
-        onChange={(e) => setQuery(e.target.value)} 
-        placeholder="Search..." 
-      />
-      <ul>
-        {articles.map(article => (
-          <li key={article.objectID}>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
-              {article.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div> */}
       <Header handleSearch={handleSearch} />
-
-      <Main
-        news={newsList}
-        hiddenNews={hiddenNews}
-        setHiddenNews={setHiddenNews}
-      />
-
+      <Main news={newsList} hiddenNews={hiddenNews} setHiddenNews={setHiddenNews} />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
